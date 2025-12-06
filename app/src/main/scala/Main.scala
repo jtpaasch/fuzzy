9f7c886ce63d30083f71873cef4365dd4e05164b
JT Paasch <jt.paasch@gmail.com>
2025-12-05 23:15:30 -0500

    Removed unnecessary imports

--- app/src/main/scala/Main.scala
+++ app/src/main/scala/Main.scala
@@ -1,12 +1,5 @@
 package app
 
-import fuzzy.Core.{Set, Real, Logic}
-import fuzzy.Set
-import fuzzy.Formula
-import fuzzy.Semantics
-import fuzzy.Dsl.*
-import fuzzy.Parser
-
 import app.Repl
 
 @main def Demo(): Unit = Repl.run
